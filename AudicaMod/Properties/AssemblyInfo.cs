e70b5b006a1c6c62d4f562687ce2cd257d527b9b
Meeps <cummies@meeps.me>
2020-09-12 00:31:33 -0700

    Initial commit
    This is version 1.0.0 of the mod! YAY

---
+++ AudicaMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using AudicaModding;
+
+[assembly: AssemblyTitle("Custom Difficulty Names")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MeepsKitten")]
+[assembly: AssemblyProduct("Custom Difficulty Names Mod")]
+[assembly: AssemblyCopyright("Created by MeepsKitten")]
+[assembly: AssemblyTrademark("MeepsKitten")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(CustomDifficultyNames), "Custom Difficulty Names", "1.0.0", "MeepsKitten")]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Harmonix Music Systems, Inc.", "Audica")]
